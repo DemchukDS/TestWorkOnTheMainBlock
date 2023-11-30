ceeaae6ec946c088888f49c31667e7bec03fdb17
DemchukDS <demchuk.btg@gmail.com>
2023-11-30 18:24:16 +0300

    Поиск количества элементов в массиве, которые меньше либо равны 3

--- Program.cs
+++ Program.cs
@@ -3,19 +3,28 @@
 Console.Write("Введите количество элементов массива: ");
 int numberOfelements = Convert.ToInt32(Console.ReadLine());
 string[] newArray = new string[numberOfelements];
-
-string[] FillArray(string[] array, int number)
+for (int i = 0; i < numberOfelements; i++)
 {
-    for (int i = 0; i < number; i++)
-    {
-        Console.Write($"Введите {i + 1} элемент массива: ");
-        array[i] = Console.ReadLine();
-    }
-    return array;
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    newArray[i] = Console.ReadLine();
 }
 
-string fillArray = string.Join(" ", FillArray(newArray, numberOfelements));
+string fillArray = string.Join(" ", newArray);
+Console.WriteLine("\nВведенный вами масив данных: ");
 Console.WriteLine(fillArray);
+
+string[] NewArrayWithElementsThetLessThenThree(string[] array)
+{
+    int sumOfElementsThatLessThanThree = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            sumOfElementsThatLessThanThree++;
+        }
+    }
+}
+
 
 Console.ReadKey();
 

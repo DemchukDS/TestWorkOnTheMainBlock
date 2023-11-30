e5c285366cdf810d310f9dee42ef04f0e51587eb
DemchukDS <demchuk.btg@gmail.com>
2023-11-30 17:55:51 +0300

    Проверка работоспособности цикла заполнения массива с консоли

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int numberOfelements = Convert.ToInt32(Console.ReadLine());
+string[] newArray = new string[numberOfelements];
+for (int i = 0; i < numberOfelements; i++)
+{
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    newArray[i] = Console.ReadLine();
+}
+string fillArray = string.Join(" ", newArray);
+Console.WriteLine(fillArray);
+
+Console.ReadKey();
